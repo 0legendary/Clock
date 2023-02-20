88b7ae31011ff09bb7cf7492c06686733fe77035
Alen.m <alenmullassery123@gmail.com>
2023-02-20 18:50:11 +0530

    Added the key and gives command for it

--- Calculator..py
+++ Calculator..py
@@ -6,4 +6,61 @@ window.title("Calculator")
 window.geometry('700x750')
 window.config(bg="gray")
 
+
+box: Entry = Entry(window, width=77, font=('Arial', 12), justify=RIGHT)
+box.grid(row=0, columnspan=5, padx=0, pady=50, ipadx=0, ipady=20, )
+
+button7 = Button(text="7", height=7, width=13, foreground='red', command=lambda: button_click(7))
+button7.grid(row=7, column=0, padx=5, pady=5)
+
+button8 = Button(text="8", height=7, width=13, foreground='red', command=lambda: button_click(8))
+button8.grid(row=7, column=1, padx=5, pady=5)
+
+button9 = Button(text="9", height=7, width=13, foreground='red',  command=lambda: button_click(9))
+button9.grid(row=7, column=2, padx=5, pady=5)
+
+button6 = Button(text="6", height=7, width=13, foreground='red', command=lambda: button_click(6))
+button6.grid(row=8, column=2, padx=5, pady=5)
+
+button5 = Button(text="5", height=7, width=13, foreground='red', command=lambda: button_click(5))
+button5.grid(row=8, column=1, padx=5, pady=5)
+
+button4 = Button(text="4", height=7, width=13, foreground='red', command=lambda: button_click(4))
+button4.grid(row=8, column=0, padx=5, pady=5)
+
+button3 = Button(text="3", height=7, width=13, foreground='red', command=lambda: button_click(3))
+button3.grid(row=9, column=2, padx=5, pady=5)
+
+button2 = Button(text="2", height=7, width=13, foreground='red', command=lambda: button_click(2))
+button2.grid(row=9, column=1, padx=5, pady=5)
+
+button1 = Button(text="1", height=7, width=13, foreground='red', command=lambda: button_click(1))
+button1.grid(row=9, column=0, padx=5, pady=5)
+
+button_dot = Button(text=".", height=7, width=13, foreground='red', command=lambda: button_click("."))
+button_dot.grid(row=10, column=0, padx=5, pady=5)
+
+button0 = Button(text="0", height=7, width=13, foreground='red', command=lambda: button_click(0))
+button0.grid(row=10, column=1, padx=5, pady=5)
+
+button_equal = Button(text="=", height=7, width=31, command=button_equal)
+button_equal.grid(row=10, column=2, columnspan=2,  padx=5, pady=5)
+
+button_plus = Button(text="+", height=16, width=13, command=button_add)
+button_plus.grid(row=9, column=4, rowspan=3, padx=5, pady=5)
+
+button_sub = Button(text="-", height=7, width=13, command=button_subtract)
+button_sub.grid(row=8, column=3, padx=5, pady=5)
+
+button_mult = Button(text="X", height=7, width=13, command=button_mult)
+button_mult.grid(row=9, column=3, padx=5, pady=5)
+
+button_div = Button(text="/", height=7, width=13, command=button_div)
+button_div.grid(row=7, column=3, padx=5, pady=5)
+
+button_clear = Button(text="C", height=16, width=13, command=button_clear)
+button_clear.grid(row=7, column=4, rowspan=2, columnspan=2, padx=5, pady=5)
+
+
+
 window.mainloop()
